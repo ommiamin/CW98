60ad4858613e5aa91b528c5d9b47ec817c3fc817
amin ommi <ommiamin@gmail.com>
2023-07-14 14:26:14 +0330

    make file 2

--- Blogapp/urls.py
+++ Blogapp/urls.py
@@ -4,5 +4,11 @@ from  . import views
 urlpatterns = [
     path('', views.home ,name='home'),
     path('detail/<int:Post_id>' , views.detail , name='details'),
-    path('delete/<int:post_id/', views.delete , name='delete')
+    path('delete/<int:post_id>', views.delete , name='delete'),
+    path('post_list/', views.post_list , name='post_list'),
+    path('authors/', views.author_list , name='author_list'),
+    path('author/<int:Author_id>', views.author , name='authors'),
+    path('category/', views.category_list , name='category_list'),
+    path('category/<int:Author_id>', views.category , name='category'),
+
 ]
--- Blogapp/views.py
+++ Blogapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render ,redirect
-from .models import Post
+from .models import Post,Author,Category
 #from django.http import HttpResponse
 
 # Create your views here.
@@ -17,3 +17,26 @@ def detail(request , Post_id):
 def delete(request , post_id):
     Post.objects.get(id=post_id).delete()
     return redirect('home')
+
+
+def post_list(request):
+    all =Post.objects.all()
+    return render(request , 'post_list.html' , {'all':all})
+
+def author_list(request):
+    all =Author.objects.all()
+    return render(request , 'author_list.html' , {'all':all})
+
+def author(request , Author_id):
+    author = Author.objects.get(id= Author_id)
+    return render(request , 'author.html',{'author':author})
+
+
+def category_list(request):
+    all =Category.objects.all()
+    return render(request , 'category_list.html' , {'all':all})
+
+def category(request , Category_id):
+    category = Category.objects.get(id= Category_id)
+    return render(request , 'category.html',{'category':category})
+    
